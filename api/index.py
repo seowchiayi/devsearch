0c16c4826da65f722b9ba579d29fedaeb5e801f4
seowchiayi <seowchiayi@gmail.com>
2024-10-24 16:13:26 +0700

    deploying nextjs flask app on vercel for the first time

--- api/index.py
+++ api/index.py
@@ -4,6 +4,22 @@ from faqGenerator import rate_limit_exceeded
 
 app = Flask(__name__)
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sys
+
+class GETHandler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!\n'.encode('utf-8'))
+        python_version = f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
+        self.wfile.write(f'Python version {python_version}'.encode('utf-8'))
+
+# variable required by Vercel
+handler = GETHandler
+
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
     return "Hello, World!"
