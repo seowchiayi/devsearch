d6623bf82c60458afcd9b7e6ff0b8864f7b608de
seowchiayi <seowchiayi@gmail.com>
2024-10-24 18:30:53 +0700

    temporarily merge faqGenerator.py with index.py

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5328/api/:path*'
-            : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/`,
+            : '/api/',
       },
     ]
   },
