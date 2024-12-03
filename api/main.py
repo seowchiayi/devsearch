79bd7f367e55ae08ececb98e45d4a3dfb6d711d5
seowchiayi <seowchiayi@gmail.com>
2024-12-03 13:16:22 +0700

    allow NEXT_PUBLIC_URL to talk to fastapi server

--- api/main.py
+++ api/main.py
@@ -11,19 +11,19 @@ import os
 import re
 import markdown
 
-
+load_dotenv()  # load environment variables
 app = FastAPI()
 
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow the Next.js dev server
+    allow_origins=["http://localhost:3000", os.getenv("NEXT_PUBLIC_URL")],  # Allow the Next.js dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-load_dotenv()  # load environment variables
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # initialize github api
